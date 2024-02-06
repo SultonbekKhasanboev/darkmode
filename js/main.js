db5c6ef95f261624b491c3b36abce5e36c3fbd3f
SultonbekKhasanboev <sultonbekkhasanboev@gmail.com>
2024-02-06 05:38:05 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var elTitle = document.querySelector('.title')
+var elBody = document.querySelector('body')
+function fn(){
+    elBody.style.background ='black'
+    elTitle.style.color = '#eee' 
+}
+function fn2(){
+    elBody.style.background ='#eee'
+    elTitle.style.color = 'black'
+}
